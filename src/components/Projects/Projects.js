eb58bb785f5501150915b6e080a7b7dbcc65f71e
Anuj Singh Rawat <84454648+anujsinghrawat@users.noreply.github.com>
2022-10-31 23:12:30 +0530

    init
    Signed-off-by: Anuj Singh Rawat <84454648+anujsinghrawat@users.noreply.github.com>

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import leaf from "../../Assets/Projects/leaf.png";
+import emotion from "../../Assets/Projects/emotion.png";
+import editor from "../../Assets/Projects/codeEditor.png";
+import chatify from "../../Assets/Projects/chatify.png";
+import suicide from "../../Assets/Projects/suicide.png";
+import weather from "../../Assets/Projects/weather.png";
+import bitsOfCode from "../../Assets/Projects/blog.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="User-Management-System"
+              description="•Crud app made with the help of Nodejs with MongoDB forthe database.
+              
+              • Successfully developed multiple REST APIs to perform all the CRUD
+              operations and fetch data from the no-sql database.
+              • The backend Server was created with the help of ExpressJs.
+              • Used Postman forthe purpose of API testing."
+              ghLink="https://github.com/anujsinghrawat/crud_app"
+              // demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bitsOfCode}
+              isBlog={false}
+              title="SuperChat-WebApp"
+              description="• A RealTime chat app made with the help of Reactjs forthe frontend and
+              Firebase forthe backend.
+              • Uses Google Auth forthe user creation, verification ,creating user avatars
+              and more.
+              • Use cloud firestore to get and post data in realtime.
+              • Ban users if not abiding by community guidelines in ChatRoom multiple
+              times"
+              ghLink="https://github.com/anujsinghrawat/superchat"
+              // demoLink="https://blogs.soumya-jit.tech/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={weather}
+              isBlog={false}
+              title="REACT-WEATHER WEBAPP"
+              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
+              ghLink="https://github.com/anujsinghrawat/weatherapp"
+              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+            />
+          </Col>
+
+          {/* <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={emotion}
+              isBlog={false}
+              title="Face Recognition and Emotion Detection"
+              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
+              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
+              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+            />
+          </Col> */}
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
